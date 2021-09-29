e982c1e2aac70e67f19c231100485a20787a7016
HoustonPL <dkreglicki@gmail.com>
2021-09-29 18:24:11 +0200

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Houston, we have a problem!'));
+app.get('/', (req, res) => res.send('GitHub, we have a problem!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
